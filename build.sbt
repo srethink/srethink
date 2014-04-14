38fb95c9c790aee2d6216409e4d5d691a9544113
jilen <jilen.zhang@gmail.com>
2014-04-14 21:52:43 +0800

    comiit

--- build.sbt
+++ build.sbt
@@ -5,5 +5,5 @@ name := "srethink"
 version := "0.0.1-SNAPSHOT"
 
 libraryDependencies ++= Seq(
-  "com.typesafe.akka" %% "akka-actor" % "2.3.2",
+  "io.netty" % "netty" % "3.9.0.Final",
   "net.sandrogrzicic" %% "scalabuff-runtime" % "1.3.7")
--- src/main/scala/srethink/io/Connection.scala
+++ src/main/scala/srethink/io/Connection.scala
@@ -1,27 +1,5 @@
 package srethink.io
 
-import akka.actor.{ Actor, ActorRef, Props }
-import akka.io._
-import akka.util.ByteString
-import java.net.InetSocketAddress
-import srethink.core._
-
-class Connection(config: HostConfig) extends Actor with ActorLogging {
-
-  def connect() = {
-    import Tcp._
-    import context.system
-    val address = InetSocketAddress(config.hostname, config.port)
-    IO(Tcp) ! Connect(address)
-  }
-
-  def receive = {
-    case CommandFailed(_: Connect) =>
-      log.info(s"connect to ${config} failed, shutdown actor!")
-      context stop self
-    case Connected(remote, local) =>
-      log.info(s"connect to ${config} success, begin event loop")
-      context.become
-  }
-
+trait Connection {
+  def connect(config: HostConfig)
 }
