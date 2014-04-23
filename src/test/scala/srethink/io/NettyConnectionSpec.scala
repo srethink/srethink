60c691e8646f2d7fa467eb1c8f8ac85f4dba85a8
jilen <jilen.zhang@gmail.com>
2014-04-23 16:34:14 +0800

    fix connection implementation

---
+++ src/test/scala/srethink/io/NettyConnectionSpec.scala
@@ -0,0 +1,95 @@
+package srethink.io
+
+import org.specs2.mutable.Specification
+import org.specs2.specification._
+import srethink.core._
+import srethink.protocol._
+import java.util.concurrent.Executors
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.Await
+import scala.concurrent.duration._
+import scala.collection.immutable._
+
+class NettyConnectionSpec extends Specification {
+
+  val tokenGen = new java.util.concurrent.atomic.AtomicLong
+
+  val cfg = NettyRethinkConfig(
+    hostname = "localhost",
+    port = 28016,
+    magic = VersionDummy.Version.V0_2_VALUE,
+    authenticationKey = "",
+    bossExecutor = Executors.newCachedThreadPool(),
+    workerExecutor = Executors.newCachedThreadPool(),
+    executionContext = global
+  )
+
+  trait WithConn extends Scope {
+    val connection = new NettyConnection(cfg)
+    connection.connect()
+  }
+
+
+  def createDb(name: String) = {
+    Query(
+      `token` = Some(tokenGen.incrementAndGet()),
+      `type` = Some(Query.QueryType.START),
+      `query` = Some(
+        Term(
+          `type` = Some(Term.TermType.DB_CREATE),
+          `args` = Seq(
+            Term(
+              `type` = Some(Term.TermType.DATUM),
+              `datum` = Some(
+                Datum(
+                  `type` = Some(Datum.DatumType.R_STR),
+                  `rStr` = Some(name)
+                )
+              )
+            )
+          )
+        )
+      )
+    )
+  }
+
+  def dropDb(name: String) = {
+    Query(
+      `token` = Some(tokenGen.incrementAndGet()),
+      `type` = Some(Query.QueryType.START),
+      `query` = Some(
+        Term(
+          `type` = Some(Term.TermType.DB_DROP),
+          `args` = Seq(
+            Term(
+              `type` = Some(Term.TermType.DATUM),
+              `datum` = Some(
+                Datum(
+                  `type` = Some(Datum.DatumType.R_STR),
+                  `rStr` = Some(name)
+                )
+              )
+            )
+          )
+        )
+      )
+    )
+  }
+
+  "connection" should  {
+    "connect to server" in new WithConn {
+      //wait connection success
+      Thread.sleep(100)
+      connection.isConnected must beEqualTo(true)
+    }
+
+    "create db" in new WithConn {
+      val createFut = connection.query(createDb("test_123"))
+      val createR = Await.result(createFut, Duration.Inf)
+      createR.`type`.get must beEqualTo(Response.ResponseType.SUCCESS_ATOM)
+
+      val dropFut = connection.query(dropDb("test_123"))
+      val dropR = Await.result(dropFut, Duration.Inf)
+    }
+  }
+}
