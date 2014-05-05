ee31c91e541428fb82e80c8e1508fd75a4515faa
jilen <jilen.zhang@gmail.com>
2014-05-05 20:04:05 +0800

    backport to 2.10

--- build.sbt
+++ build.sbt
@@ -7,9 +7,6 @@ version := "0.0.1-SNAPSHOT"
 libraryDependencies ++= Seq(
   "io.netty" % "netty" % "3.9.0.Final",
   "net.sandrogrzicic" %% "scalabuff-runtime" % "1.3.7",
-  "org.scalamacros" %% "quasiquotes" % "2.0.0",
   "org.specs2" %% "specs2" % "2.3.11" % "test")
 
 scalacOptions ++= Seq("-feature", "-deprecation")
-
-addCompilerPlugin("org.scalamacros" % "paradise" % "2.0.0" cross CrossVersion.full)
--- src/main/scala/srethink/core/codec/Codecs.scala
+++ src/main/scala/srethink/core/codec/Codecs.scala
@@ -1,8 +1,112 @@
 package srethink.core.codec
 
-import scala.language.experimental.macros
+import srethink.protocol._
 import srethink.core._
+import CodecHelper._
 
-object Codecs {
-  def encoder[T]: DatumEncoder[T] = macro CodecsMacro.encoderImpl[T]
+
+trait BaseDatumDecoders {
+
+  implicit object IntDecoder extends DatumDecoder[Int] {
+    def decode(datum: Datum) = {
+      for {
+        t <- datum.`type` if t == Datum.DatumType.R_NUM
+        v <- datum.rNum
+      } yield v.toInt
+    }
+  }
+
+  implicit object StringDecoder extends DatumDecoder[String] {
+    def decode(datum: Datum) = {
+      for {
+        t <- datum.`type` if t == Datum.DatumType.R_STR
+        v <- datum.rStr
+      } yield v
+    }
+  }
+
+  implicit object LongDecoder extends DatumDecoder[Long] {
+    def decode(datum: Datum) = {
+      for {
+        t <- datum.`type` if t == Datum.DatumType.R_NUM
+        v <- datum.rNum
+      } yield v.toLong
+    }
+  }
+
+  implicit object BooleanDecoder extends DatumDecoder[Boolean] {
+    def decode(datum: Datum) = {
+      for {
+        t <- datum.`type` if t == Datum.DatumType.R_BOOL
+        v <- datum.rBool
+      } yield v
+    }
+  }
+
 }
+
+trait BaseDatumEncoders {
+
+  implicit object IntEncoder extends DatumEncoder[Int] {
+    def encode(t: Int) = {
+      numDatum(t)
+    }
+  }
+
+  implicit object StringEncoder extends DatumEncoder[String] {
+    def encode(t: String) = {
+      strDatum(t)
+    }
+  }
+
+  implicit object LongEncoder extends DatumEncoder[Long] {
+    def encode(t: Long) = {
+      numDatum(t)
+    }
+  }
+
+  implicit object BooleanEncoder extends DatumEncoder[Boolean] {
+    def encode(t: Boolean) = {
+      boolDatum(t)
+    }
+  }
+
+  def encode[T: DatumEncoder](t: T) = {
+    implicitly[DatumEncoder[T]].encode(t)
+  }
+
+  def encodeBaseType(t: Any) = t match {
+    case v: Int => encode(v)
+    case v: String => encode(v)
+    case v: Long => encode(v)
+    case v: Boolean => encode(v)
+  }
+}
+
+trait CaseDatumEncoders extends BaseDatumEncoders {
+  import scala.collection.immutable
+
+  class CaseEncoder[T]( naming: String => String) extends DatumEncoder[T] {
+    def encode(t: T) = {
+      objDatum(pairsOf(t))
+    }
+
+    //Not using reflect api for thread-safety issue when 2.10
+    private def pairsOf(t: T): immutable.Seq[Datum.AssocPair] = {
+      t.getClass.getDeclaredFields.map { f =>
+        f.setAccessible(true)
+        Datum.AssocPair(
+          Some(naming(f.getName)),
+          Some(encodeBaseType(f.get(t)))
+        )
+      }.to[immutable.Seq]
+    }
+  }
+}
+
+trait CaseDatumDecoders extends BaseDatumDecoders {
+  class CaseDecoder[T](naming: String => String) extends DatumDecoder[T] {
+  }
+}
+
+object Codecs extends CaseDatumEncoders with BaseDatumEncoders with BaseDatumDecoders
