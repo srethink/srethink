1d8e468ca965ba606ea9764549a5eddcc24b1dad
jilen <jilen.zhang@gmail.com>
2015-11-30 14:42:22 +0800

    add RVar

--- src/main/scala/org/srethink/ast/RFunc.scala
+++ src/main/scala/org/srethink/ast/RFunc.scala
@@ -1,7 +1,9 @@
 package org.srethink.ast
 
 import io.circe._
+import org.srethink.ast.Helper._
+import org.srethink.net._
 
 case class RVar(id: Int) extends Ast {
-  def toTerm = Json.array()
+  def toTerm = term(TermConstant.VAR_VALUE, Seq(Json.int(id)), Nil)
 }
