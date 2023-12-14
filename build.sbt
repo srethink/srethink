bdabf8654907619b5bb947f0dac8e797d0ee3538
jilen <jilen.zhang@gmail.com>
2023-12-14 12:08:37 +0800

    stick to play 2.5.x

--- build.sbt
+++ build.sbt
@@ -6,24 +6,18 @@ scalaVersion := "2.11.12"
 
 crossScalaVersions := Seq("2.11.12", "2.12.18", "2.13.12")
 
-def playJsonV(v: String) = {
-  if (v.startsWith("2.11"))
-    "2.7.4"
-  else "2.10.3"
-
-}
+val playJsonV    = "2.5.19"
+val slf4jVersion = "1.7.36"
 
 libraryDependencies ++= {
   val nettyV = "3.10.6.Final"
   Seq(
-    "io.netty"           % "netty"    % nettyV,
-    "co.fs2"            %% "fs2-core" % "2.0.0",
-    "com.typesafe.play" %% "play-json" % playJsonV(
-      scalaVersion.value
-    )                % Provided,
-    "org.scalameta" %% "munit"        % "1.0.0-M10" % Test,
-    "org.slf4j"      % "slf4j-api"    % "1.7.22"    % "provided",
-    "org.slf4j"      % "slf4j-simple" % "1.7.22"    % "test"
+    "io.netty"           % "netty"        % nettyV,
+    "co.fs2"            %% "fs2-core"     % "2.0.0",
+    "org.slf4j"          % "slf4j-api"    % slf4jVersion,
+    "com.typesafe.play" %% "play-json"    % playJsonV    % Provided,
+    "org.scalameta"     %% "munit"        % "1.0.0-M10"  % Test,
+    "org.slf4j"          % "slf4j-simple" % slf4jVersion % Test
   )
 }
 
--- src/main/scala/play/api/rethink/PlayRethinkFormats.scala
+++ src/main/scala/play/api/rethink/PlayRethinkFormats.scala
@@ -3,13 +3,12 @@ package play.api.rethink
 import java.util.Date
 import play.api.libs.json._
 import play.api.libs.json.{JsValue => JS}
-import play.api.libs.json.JsonValidationError
 import srethink.ast._
 import srethink.protocol.TermConstant.MAKE_ARRAY_VALUE
 
 trait PlayRethinkFormats {
 
-  implicit def eitherReads[A: Reads, B: Reads]: Reads[Either[A, B]] =  {
+  implicit def eitherReads[A: Reads, B: Reads]: Reads[Either[A, B]] = {
     new Reads[Either[A, B]] {
       def reads(json: JS) = {
         Json.fromJson[A](json).map(Left[A, B](_)).orElse {
@@ -23,39 +22,40 @@ trait PlayRethinkFormats {
     new Writes[Either[A, B]] {
       def writes(e: Either[A, B]) = {
         e match {
-          case Left(a) => Json.toJson(a)
+          case Left(a)  => Json.toJson(a)
           case Right(b) => Json.toJson(b)
         }
       }
     }
   }
 
-  private val readRqlTime : (String, Double) => JsResult[Date] = {
+  private val readRqlTime: (String, Double) => JsResult[Date] = {
     case (rqlType, epochTime) =>
-      if(rqlType != "TIME") {
-        JsError(Seq(JsPath() -> Seq(JsonValidationError("error.expected.rql_type to be TIME"))))
+      if (rqlType != "TIME") {
+        JsError("error.expected.rql_type to be TIME")
       } else {
         JsSuccess(new Date((epochTime * 1000).toLong))
       }
   }
 
-  implicit val timeReads : Reads[Date] = new Reads[Date] {
+  implicit val timeReads: Reads[Date] = new Reads[Date] {
     def reads(json: JS) = {
       json match {
-        case JsObject(props) if props.get("$reql_type$") == Some(JsString("TIME")) =>
+        case JsObject(props)
+            if props.get("$reql_type$") == Some(JsString("TIME")) =>
           val JsNumber(t) = props("epoch_time")
           JsSuccess(new Date((t * 1000).toLong))
       }
     }
   }
 
-  implicit val timeWrites : Writes[Date] = new Writes[Date] {
+  implicit val timeWrites: Writes[Date] = new Writes[Date] {
     def writes(date: Date) = {
       JsObject(
         Seq(
           "$reql_type$" -> JsString("TIME"),
-          "epoch_time" -> JsNumber(BigDecimal(date.getTime) / 1000),
-          "timezone" -> JsString("+00:00")
+          "epoch_time"  -> JsNumber(BigDecimal(date.getTime) / 1000),
+          "timezone"    -> JsString("+00:00")
         )
       )
     }
@@ -63,14 +63,14 @@ trait PlayRethinkFormats {
 
   lazy implicit val insertRFormat = Json.format[InsertResult]
   lazy implicit val createRFormat = Json.format[CreateResult]
-  lazy implicit val dropRFormat = Json.format[DropResult]
+  lazy implicit val dropRFormat   = Json.format[DropResult]
   lazy implicit val deleteRFormat = Json.format[DeleteResult]
   lazy implicit val updateRFormat = Json.format[UpdateResult]
-  lazy implicit val booleanF = implicitly[Format[Boolean]]
-  lazy implicit val intF = implicitly[Format[Int]]
-  lazy implicit val longF = implicitly[Format[Long]]
-  lazy implicit val floatF = implicitly[Format[Float]]
-  lazy implicit val doubleF = implicitly[Format[Double]]
-  lazy implicit val stringF = implicitly[Format[String]]
-  lazy implicit val dateF = implicitly[Format[Date]]
+  lazy implicit val booleanF      = implicitly[Format[Boolean]]
+  lazy implicit val intF          = implicitly[Format[Int]]
+  lazy implicit val longF         = implicitly[Format[Long]]
+  lazy implicit val floatF        = implicitly[Format[Float]]
+  lazy implicit val doubleF       = implicitly[Format[Double]]
+  lazy implicit val stringF       = implicitly[Format[String]]
+  lazy implicit val dateF         = implicitly[Format[Date]]
 }
