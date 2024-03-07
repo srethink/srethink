ee1c623434f6205c4333e7095ad9c433cfd37463
jilen <jilen.zhang@gmail.com>
2024-03-07 16:27:26 +0800

    try fix cursor

--- src/main/scala/srethink/net/NettyConnection.scala
+++ src/main/scala/srethink/net/NettyConnection.scala
@@ -13,15 +13,21 @@ import org.jboss.netty.util._
 import org.slf4j.LoggerFactory
 import srethink.protocol.Protocol
 
-
 class RethinkTimer(val interval: FiniteDuration) {
-  val timer = new HashedWheelTimer(interval.toMillis, java.util.concurrent.TimeUnit.MILLISECONDS)
+  val timer = new HashedWheelTimer(
+    interval.toMillis,
+    java.util.concurrent.TimeUnit.MILLISECONDS
+  )
   def newSchedule(delay: FiniteDuration)(f: () => Unit) = {
-    timer.newTimeout(new TimerTask {
-      def run(timeout: Timeout) {
-        f()
-      }
-    }, delay.toMillis, java.util.concurrent.TimeUnit.MILLISECONDS)
+    timer.newTimeout(
+      new TimerTask {
+        def run(timeout: Timeout) {
+          f()
+        }
+      },
+      delay.toMillis,
+      java.util.concurrent.TimeUnit.MILLISECONDS
+    )
   }
 
   def stop() = timer.stop()
@@ -31,14 +37,14 @@ object DefaultRethinkTimer extends RethinkTimer(1.seconds)
 
 class NettyConnection(val config: NettyRethinkConfig) extends Connection {
 
-  private val logger = LoggerFactory.getLogger(classOf[Connection])
+  private val logger  = LoggerFactory.getLogger(classOf[Connection])
   private val charset = java.nio.charset.Charset.defaultCharset()
   private implicit val executionContext = config.executionContext
   @volatile
-  private var channel = None: Option[Channel]
+  private var channel     = None: Option[Channel]
   private val responseMap = TrieMap[Long, Promise[Response]]()
-  private val handshake = Promise[String]()
-  private val timer = config.timer
+  private val handshake   = Promise[String]()
+  private val timer       = config.timer
 
   def close() = channel.foreach { c =>
     c.close()
@@ -52,36 +58,40 @@ class NettyConnection(val config: NettyRethinkConfig) extends Connection {
     logger.debug("sending query {}", query)
     val newPromise = Promise[Response]()
     def p = responseMap.putIfAbsent(query.token, newPromise).getOrElse {
-      channel.get.write(query).addListener(new ChannelFutureListener {
-        def operationComplete(f: ChannelFuture) {
-          if(!f.isSuccess()) {
-            newPromise.tryComplete(Failure(f.getCause()))
+      channel.get
+        .write(query)
+        .addListener(new ChannelFutureListener {
+          def operationComplete(f: ChannelFuture) {
+            if (!f.isSuccess()) {
+              newPromise.tryComplete(Failure(f.getCause()))
+            }
           }
-        }
-      })
+        })
       newPromise
     }
     val timeout = timer.newSchedule(config.requestTimeout) { () =>
-      if(!p.isCompleted) {
+      if (!p.isCompleted) {
         p.tryComplete(Failure(new java.util.concurrent.TimeoutException()))
       }
     }
     for {
       shake <- handshake.future
-      resp <- p.future.andThen{ case _ => timeout.cancel() }
+      resp  <- p.future.andThen { case _ => timeout.cancel() }
     } yield resp
   }
 
   def connect() = {
     logger.info("connecting to host {}", config.hostname)
-    val channelFuture = bootstrap().connect(new java.net.InetSocketAddress(config.hostname, config.port))
+    val channelFuture = bootstrap().connect(
+      new java.net.InetSocketAddress(config.hostname, config.port)
+    )
     channel = Some(channelFuture.getChannel)
     Await.ready(handshake.future, 3.seconds)
   }
 
   private def markAllFail(ex: Throwable) {
     logger.error("Error caught, set all futures to fail", ex)
-    val keys = for( (k, v) <- responseMap if(!v.isCompleted)) yield {
+    val keys = for ((k, v) <- responseMap if (!v.isCompleted)) yield {
       v.tryComplete(Failure(ex))
       k
     }
@@ -90,11 +100,13 @@ class NettyConnection(val config: NettyRethinkConfig) extends Connection {
 
   private def bootstrap() = {
     val bootstrap = new ClientBootstrap(config.channelFactory)
-    val bufferFactory = new HeapChannelBufferFactory(java.nio.ByteOrder.LITTLE_ENDIAN)
+    val bufferFactory = new HeapChannelBufferFactory(
+      java.nio.ByteOrder.LITTLE_ENDIAN
+    )
     bootstrap.setOption("bufferFactory", bufferFactory)
     bootstrap.setOption("child.keepAlive", true)
-    bootstrap.setOption("connectTimeoutMillis",config.connectTimeout.toMillis)
-    //Set pipeline factory
+    bootstrap.setOption("connectTimeoutMillis", config.connectTimeout.toMillis)
+    // Set pipeline factory
     val pipelineFactory = new ChannelPipelineFactory {
       def getPipeline() = {
         Channels.pipeline(new RethinkHandler)
@@ -105,12 +117,15 @@ class NettyConnection(val config: NettyRethinkConfig) extends Connection {
   }
 
   class RethinkHandler extends SimpleChannelUpstreamHandler {
-    private val frameDecoderName = "frameDecoder"
+    private val frameDecoderName   = "frameDecoder"
     private val messageDecoderName = "messageDecoder"
-    private val frameEncoderName = "frameEncoder"
+    private val frameEncoderName   = "frameEncoder"
     private val messageEncoderName = "messageEncoder"
 
-    override def exceptionCaught(ctx: ChannelHandlerContext, e: ExceptionEvent) = {
+    override def exceptionCaught(
+      ctx: ChannelHandlerContext,
+      e: ExceptionEvent
+    ) = {
       e.getCause().printStackTrace()
       e.getCause() match {
         case e: java.net.ConnectException =>
@@ -121,7 +136,10 @@ class NettyConnection(val config: NettyRethinkConfig) extends Connection {
       }
     }
 
-    override def channelConnected(ctx: ChannelHandlerContext, e: ChannelStateEvent) {
+    override def channelConnected(
+      ctx: ChannelHandlerContext,
+      e: ChannelStateEvent
+    ) {
       prepareHandshake(ctx)
       sendHandshake(e.getChannel)
     }
@@ -135,7 +153,9 @@ class NettyConnection(val config: NettyRethinkConfig) extends Connection {
         case handshakeResult: String =>
           handshake.failure(new ConnectionError(handshakeResult.toString))
         case response: Response =>
-          responseMap.get(response.token).foreach(_.tryComplete(Success(response)))
+          responseMap
+            .get(response.token)
+            .foreach(_.tryComplete(Success(response)))
           responseMap.remove(response.token)
           logger.debug("response return for {}", response)
       }
@@ -143,23 +163,34 @@ class NettyConnection(val config: NettyRethinkConfig) extends Connection {
 
     private def prepareQuery(ctx: ChannelHandlerContext) {
       val pipeline = ctx.getPipeline()
-      pipeline.replace(frameDecoderName, frameDecoderName, new LengthFieldBasedFrameDecoder(8 * 1024 * 1024, 8, 4))
-      pipeline.replace(messageDecoderName, messageDecoderName, new ResponseDecoder(charset))
+      pipeline.replace(
+        frameDecoderName,
+        frameDecoderName,
+        new LengthFieldBasedFrameDecoder(8 * 1024 * 1024, 8, 4)
+      )
+      pipeline.replace(
+        messageDecoderName,
+        messageDecoderName,
+        new ResponseDecoder(charset)
+      )
       pipeline.addFirst(messageEncoderName, new QueryEncoder(charset))
     }
 
     private def prepareHandshake(ctx: ChannelHandlerContext) {
       val pipeline = ctx.getPipeline()
       pipeline.addFirst(messageDecoderName, new StringDecoder)
-      pipeline.addFirst(frameDecoderName, new DelimiterBasedFrameDecoder(1024, Delimiters.nulDelimiter: _*))
+      pipeline.addFirst(
+        frameDecoderName,
+        new DelimiterBasedFrameDecoder(1024, Delimiters.nulDelimiter: _*)
+      )
     }
 
     private def sendHandshake(channel: Channel) {
       import config._
       val authKey = authenticationKey.getBytes("ascii")
-      //4 bytes magic, 4 bytes key length, 4 bytes protocol
+      // 4 bytes magic, 4 bytes key length, 4 bytes protocol
       val bufSize = 4 + 4 + authKey.length + 4
-      val buf = ChannelBuffers.buffer(ChannelBuffers.LITTLE_ENDIAN, bufSize)
+      val buf     = ChannelBuffers.buffer(ChannelBuffers.LITTLE_ENDIAN, bufSize)
       buf.writeInt(magic)
       buf.writeInt(authKey.length)
       buf.writeBytes(authKey)
